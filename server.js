04a3e592a0e38f50c182cf7732097d77fbcccda4
Eric Chu <lmceric@gmail.com>
2026-05-06 16:29:26 +0100

    Deploy

--- server.js
+++ server.js
@@ -162,6 +162,7 @@ for (const psc of uniqueItems) {
     }
 });
 
-app.listen(3000, () => {
-    console.log('Server running at http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
 });
